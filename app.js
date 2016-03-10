9aeed7060ded705244e0267b5e1adab2235716b9
David R. Jones <drj@rackfx.com>
2016-03-09 19:01:44 -0700

    request weather and put it in a view

--- app.js
+++ app.js
@@ -2,24 +2,29 @@
 var express = require('express');
 var app = express();
 var ejs = require('ejs');
+var request = require('request');
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
 
-app.get('/', function (req, res) {
-  res.render('weather');
+app.get('/', function (req, res){
+  request('http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=cb5a9ed44f1d8c2013040f360b9e200d', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var weatherObject = JSON.parse(body); // Show the HTML for the Google homepage.
+
+      var data = {};
+      data.weather = weatherObject.list[0].weather;
+      data.pageTitle = "this is the weather for moscow";
+      console.log(data)
+      res.render('weather', { data: data });
+
+    }
+  })
+
+
 });
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!');
 });
-
-
-var request = require('request');
-request('http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=cb5a9ed44f1d8c2013040f360b9e200d', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var weatherObject = JSON.parse(body); // Show the HTML for the Google homepage.
-    console.log(weatherObject.list[0].weather);
-  }
-})
